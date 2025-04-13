c9c06dbfeccd0aced563453ca84ae89788643ca9
Aditya Anand Shinde🙋‍♂️ <71961975+adityashin@users.noreply.github.com>
2025-04-13 21:57:03 +0530

    Update MySQL_Functions.sql

--- MySQL_Functions.sql
+++ MySQL_Functions.sql
@@ -15,3 +15,27 @@ from emp;
 select datediff('2025-04-07',hiredate) as DateDifference
 from emp;
 
+
+select distinct(ename) as VOWEL_EMP
+from emp
+where lower(ename) regexp '^[aeiou].*[aeiou]$';
+
+insert into emp(ename) values("AEIOU");
+
+use company;
+desc emp;
+
+select deptno,count(empno) as Total_Employees
+from emp
+group by deptno;
+
+select ename,sal
+from emp
+order by sal desc;
+
+select deptno,count(empno)
+from emp
+group by deptno
+having sum(sal) > 4000;
+
+select * from emp;
